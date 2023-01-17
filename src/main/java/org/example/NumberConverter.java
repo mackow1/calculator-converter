9ff0009ebf5219d80a127d4474a2e734fdf2f424
mackow1 <mm.kowalczykk@gmail.com>
2023-01-17 15:38:09 +0100

    added method decimalToHexSwitch and decimal to hex conversion

--- src/main/java/org/example/NumberConverter.java
+++ src/main/java/org/example/NumberConverter.java
@@ -157,7 +157,44 @@ public class NumberConverter {
     }
 
     public static String decimalToHex(int decimal) {
-        return "0";
+        String hexNumber = "";
+        while (decimal != 0) {
+            if (decimal >= 4096) {
+                hexNumber += decimalToHexSwitch(decimal / 4096);
+                decimal = decimal % 4096;
+            } else if (decimal >= 256) {
+                hexNumber += decimalToHexSwitch(decimal / 256);
+                decimal = decimal % 256;
+            } else if (decimal >= 16) {
+                hexNumber += decimalToHexSwitch(decimal / 16);
+                decimal = decimal % 16;
+            } else if (decimal >= 1) {
+                hexNumber += decimalToHexSwitch(decimal);
+                decimal = 0;
+            }
+        }
+        return hexNumber;
+    }
+
+    private static String decimalToHexSwitch(int number) {
+        return switch (number) {
+            case 1 -> "1";
+            case 2 -> "2";
+            case 3 -> "3";
+            case 4 -> "4";
+            case 5 -> "5";
+            case 6 -> "6";
+            case 7 -> "7";
+            case 8 -> "8";
+            case 9 -> "9";
+            case 10 -> "A";
+            case 11 -> "B";
+            case 12 -> "C";
+            case 13 -> "D";
+            case 14 -> "E";
+            case 15 -> "F";
+            default -> "";
+        };
     }
 
     public static int hexToDecimal(String hex) {
